b29d0e6403be41261cdc28be6477bc35a86aeeb8
JoseDuarte83 <joseduartesls@hotmail.com>
2025-08-12 20:33:02 -0600

    first commit

---
+++ src/styles/Variables.js
@@ -0,0 +1,14 @@
+export const colors = {
+    primary : '#ffe81f',
+    secondary : '#3a3a3a',
+    terciary : '#1a1a1a',
+    accent : '#00bfff',
+    text : '#FFFFFF',
+    textSecondary : '#CCCCCC',
+    border : '#555555',
+};
+
+export const fonts = {
+    main : 'arial, sans-serif',
+    starwars : 'Star Jedi, sans-serif'
+};
